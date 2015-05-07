7998852cf16d48e0a381387e93a1bfa7644b2087
Shereef Bishay <shereef@gmail.com>
2015-05-06 22:30:18 -0700

    moved form to optional

--- app/controlers/RootViewController.rb
+++ app/controlers/RootViewController.rb
@@ -15,7 +15,76 @@ class RootViewController < UIViewController
     @label2.center = CGPointMake(self.view.frame.size.width / 2, (self.view.frame.size.height / 2) - 225)
 
 
-    self.view.addSubview @label1
-    self.view.addSubview @label2    
+    # self.view.addSubview @label1
+    # self.view.addSubview @label2
+    
+    show_form
+    
+  end
+  
+  def show_form
+    
+    @form = Formotion::Form.new({
+          title: "SomeOfUs",
+          sections: [{
+            title: "Welcome, let's get started!",
+            footer: "We will never spam you, or sell your data. Seriously. Ever.",
+            rows: [{
+              title: "Email",
+              key: :email,
+              placeholder: "me@mail.com",
+              image: "email",
+              type: :email,
+              auto_correction: :no,
+              auto_capitalization: :none,
+              input_accessory: :done
+            }, {
+              title: "First Name",
+              key: :first,
+              placeholder: "Jonny",
+              type: :string,
+              auto_correction: :no,
+              input_accessory: :done
+            }, {
+              title: "Last Name",
+              key: :last,
+              placeholder: "Appleseed",
+              type: :string,
+              auto_correction: :no,
+              input_accessory: :done
+            }, {
+              title: "Zip",
+              key: :zip,
+              placeholder: "12345",
+              type: :number,
+              auto_correction: :no,
+              auto_capitalization: :none,
+              input_accessory: :done
+            }]
+          }, {
+            rows: [{
+              title: "Submit",
+              type: :submit,
+            }]
+          }]
+        })
+
+      @form_controller = Formotion::FormController.alloc.initWithForm(@form)
+      @form_controller.form.on_submit do |form|
+        form.active_row && form.active_row.text_field.resignFirstResponder
+        alert = UIAlertView.alloc.init
+        alert.title = "@form.render"
+        alert.message = @form.render.to_s
+        alert.addButtonWithTitle("OK")
+        alert.show
+      end
+
+    @form_controller.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemSave, target:self, action:'submit')
+    @navigation_controller = UINavigationController.alloc.initWithRootViewController(@form_controller)
+     
+    self.presentViewController(@navigation_controller,
+    animated:true,
+    completion: lambda {})    
+    
   end
 end
